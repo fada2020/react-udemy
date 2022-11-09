f7d8bc11e65487bbfcd6efe9334e4a9a77c688e7
ihyeogju <leehyeokju1@gmail.com>
2022-11-09 12:14:00 +0900

    value input 안되는 에러 수정

--- src/components/newExpense/NewExpenseForm.js
+++ src/components/newExpense/NewExpenseForm.js
@@ -4,16 +4,16 @@ const NewExpenseForm = () =>{
     return <form>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
-                <label for="">Title</label>
-                <input type="text" name="" value=""/>
+                <label htmlFor="">Title</label>
+                <input type="text" name="" />
             </div>  
             <div className='new-expense__control'>
-                <label for="">Amount</label>
-                <input type="number" name="" min="0.01" step="0.01" value=""/>
+                <label htmlFor="">Amount</label>
+                <input type="number" name="" min="0.01" step="0.01" />
             </div>
             <div className='new-expense__control'>
-                <label for="">Date</label>
-                <input type="date" name="" min="2019-01-01" max="2022-11-02" value=""/>
+                <label htmlFor="">Date</label>
+                <input type="date" name="" min="2019-01-01" max="2022-11-02" />
             </div>
         </div>
         <div className='new-expense__actions'>
